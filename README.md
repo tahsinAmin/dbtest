5e53be14aefda2d6f5c8eb1a88dde79d35c417c8
tahsinAmin <muhammadtahsinamin@gmail.com>
2022-01-11 07:12:27 +0600

    Works from home after configuring the password

--- README.md
+++ README.md
@@ -4,5 +4,14 @@
 
 - [x] a Table created
 - [x] A table is created and also dropped
-- [ ] Check if it works after going home by configuring the password, etc if needed
-- [ ] Insert data
+- [x] Insert data
+- [x] Check if it works after going home by configuring the password, etc if needed
+- [ ] Print in tabular format => https://www.delftstack.com/howto/python/data-in-table-format-python/
+
+# What I learned
+
+- [x] About "with" clause. 1. A "with" clause will take care of closing the cursor for you. 2. We don't need to mention about connection.commit after our sql manipulation, wit clause will take of it as soon as our with clause of database connection is over, it'll comit the transaction automatic. Once we're done, It closes the cursor as well however, it doesn't close the connection of our database. so, tha needs to be done manually.
+
+# Problem and its solution
+
+- [ ] Ho to print in tabular format. Solution maybe = > https://www.delftstack.com/howto/python/data-in-table-format-python/
--- dummy.py
+++ dummy.py
@@ -1,60 +1,71 @@
 import psycopg2
+import psycopg2.extras
 
 # change password when in office
-
 # https://www.youtube.com/watch?v=M2NzvnfS-hI
 
 # connect tom the db
 conn = None
-cur = None
-try:
-    print("start")
 
 # for office
-    # conn = psycopg2.connect(
-    #     host = 'localhost',
-    #     database = 'test',
-    #     user = 'postgres',
-    #     password = 'password',
-    #     port = 5432)
+# try:
+# with psycopg2.connect(
+#     host = 'localhost',
+#     database = 'test',
+#     user = 'postgres',
+#     password = 'password',
+#     port = 5432) as conn
 
-# for home
-    conn = psycopg2.connect(
-        host='localhost',
-        database='db',
-        user='postgres',
-        password='root',
-        port=5432)
 
-    # cursor
-    cur = conn.cursor()
+try:
+    # for home
+    with psycopg2.connect(
+            host='localhost',
+            database='db',
+            user='postgres',
+            password='root',
+            port=5432) as conn:
 
-    # execute the code
-    create_script = """CREATE TABLE IF NOT EXISTS employee (
-                        id      int NOT NULL PRIMARY KEY,
-                        name    VARCHAR(40) NOT NULL,
-                        salary  int,
-                        dept_id VARCHAR(30))"""
-    cur.execute(create_script)
+        # cursor
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
 
-    insert_script = 'INSERT INTO employee (id, name, salary, dept_id) VALUES (%s, %s, %s, %s)'
-    insert_values = (1, 'James', 1200, 'D1')
-    cur.execute(insert_script, insert_values)
+            # Drop table if exist before starting insertion. this is because we donn't want error for duplicate keys.
+            drop_script = """DROP TABLE employee"""
+            cur.execute(drop_script)
 
-    # drop_script = """DROP TABLE employee"""
-    # cur.execute(drop_script)
+            # execute the code
+            create_script = """CREATE TABLE IF NOT EXISTS employee (
+                                id      int NOT NULL PRIMARY KEY,
+                                name    VARCHAR(40) NOT NULL,
+                                salary  int,
+                                dept_id VARCHAR(30))"""
+            cur.execute(create_script)
 
-    # save any transactions that we have done in the database
-    conn.commit()
+            insert_script = 'INSERT INTO employee (id, name, salary, dept_id) VALUES (%s, %s, %s, %s)'
+            insert_values = [(1, 'James', 1200, 'D1'),
+                             (2, 'Robin', 1300, 'D1'),
+                             (3, 'Xavier', 1400, 'D1')]
+            for record in insert_values:
+                cur.execute(insert_script, record)
+
+            update_script = 'UPDATE employee SET salary = salary + (salary *0.5);'
+            cur.execute(update_script)
+
+            delete_record = ('James',)
+            delete_script = 'DELETE FROM employee WHERE name = %s'
+            cur.execute(delete_script, delete_record)
+
+            print("Name | Salary")
+            print("-----+-------")
+            cur.execute('SELECT * FROM employee')
+            for record in cur.fetchall():
+                print(record['name'], record['salary'])
+
 
 except Exception as error:
     print(error)
 finally:
     # finally will execute with or wiothout any error bcz if and error occurs, the try block stops.
-
-    if cur is not None:
-        # close the cursor
-        cur.close()
 
     if conn is not None:
         # close the connection
